370c192d102012f16c5384f88e96937245e51ec5
tomaszociepa <tom.ociepa@gmail.com>
2019-07-08 18:28:07 +0200

    sum method

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//1. Write a method that sums two numbers.
+        Sum sum = new Sum();
+        System.out.println(sum.sum(2, 4));
+
+
+        
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,8 @@
+//Write a method that sums two numbers.
+public class Sum {
+
+    public int sum(int a, int b){
+
+        return a + b;
+    }
+}
